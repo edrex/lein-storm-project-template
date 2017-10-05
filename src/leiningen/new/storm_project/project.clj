9d7e91cdceec506f651f25895c26903788d05734
Eric Drechsel <eric@pdxhub.org>
2017-10-05 14:20:34 -0700

    use single-file wordcount example from storm-starter

--- src/leiningen/new/storm_project/project.clj
+++ src/leiningen/new/storm_project/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :aot [{{ns-name}}.TopologySubmitter]
+  :aot [{{ns-name}}.word-count]
   ;; include storm dependency only in dev because production storm cluster provides it
   :profiles {:dev {:dependencies [[org.apache.storm/storm-core "1.1.1"]]}
              :provided {:dependencies [[org.apache.storm/storm-core "1.1.1"]]}})
